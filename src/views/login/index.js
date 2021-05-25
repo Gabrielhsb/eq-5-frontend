8dbb8c84b55771f0f422e0d51a592af1b264f05b
Gabriel Bernardo <gabriel.bernardo@estudante.ufla.br>
2021-05-25 18:03:13 -0300

    Ajuste no login

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -11,6 +11,21 @@ export default function Login() {
           <div className={styles.loginCard}>
               <LoginCard/>
           </div>
+          <div className={styles.mobileLogin}>
+            <h1 className={styles.title}>Fazer Login</h1>
+            <p>Acesse a sua conta</p>
+            <form className={styles.form}>
+              <div className={styles.divLabel}>
+                <label >Email</label><br/>
+                <input className={styles.input}  type="email" id="email" /><br/>
+              </div>
+              <div className={styles.divLabel}>
+                <label >Senha</label><br/>
+                <input className={styles.input}  type="password" id="password"/><br/>
+              </div>
+              <input className={styles.button} type="submit" value="ENTRAR"/>
+            </form>
+          </div>
           <div className={styles.signUpCard}>
               <SignUpCard/>
           </div>
